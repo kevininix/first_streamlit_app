bbe53fe6463da2950c663c065b3f499c888cb781
kevininix <88592244+kevininix@users.noreply.github.com>
2022-08-14 17:03:32 -0600

    Update streamlit_app.py
    Added a pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# Upload data into a dataframe
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+# create a pick list for fruits
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+# Display dataframe
 streamlit.dataframe(my_fruit_list)
+
