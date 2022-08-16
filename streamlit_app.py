dc7336e29a267fd1dc3752131ae4b32ba57e5792
kevininix <88592244+kevininix@users.noreply.github.com>
2022-08-15 21:10:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,5 @@ streamlit.dataframe(fruits_to_show)
 
 # Display Fruitvice api response
 fruitvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
-streamlit.text('fruityvice_response')
+streamlit.text(fruityvice_response)
 
