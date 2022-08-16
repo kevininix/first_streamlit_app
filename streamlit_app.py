a71f98e09ba09b577637d4ba6fa410a559515ac5
kevininix <88592244+kevininix@users.noreply.github.com>
2022-08-15 21:34:07 -0600

    Update streamlit_app.py
    Got rid of raw json and separated URL from fruit name

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 # Display fruityvice api response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + 'kiwi')
+#streamlit.text(fruityvice_response.json())
 
 # Normalize json response
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
